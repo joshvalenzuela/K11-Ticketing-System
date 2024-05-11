e8a19224ba2391ba1f04ed281a0545a816666164
joshvalenzuela <140882882+joshvalenzuela@users.noreply.github.com>
2024-05-12 00:16:15 +0800

    Add files via upload

---
+++ src/moreGuiTest.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class moreGuiTest {
+    JFrame frame;
+    JPanel panel;
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/userCore.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class userCore {
+    public static Scanner sc = new Scanner(System.in);
+    public static String[] sectionA = new String[6];
+    public String[] readFile() {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("MyFile.txt"));
+            String line = reader.readLine();
+            int readcounter = 0;
+            sectionA[readcounter++] = line;
+            while ((line = reader.readLine()) != null) {
+                sectionA[readcounter++] = line;
+            }
+        } catch (IOException e) {
+            System.out.println("Error, Please contact Program Admin. : " + e);
+            System.exit(0);
+        }
+        return sectionA;
+    }
+
+    public void savetoFile(String[] arr) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter("myFile.txt", false));
+        for (String s : arr) {
+            writer.write(s);
+            writer.newLine();
+        }
+        writer.flush();
+        System.out.println("Successfully saved.");
+    }
+
+}
